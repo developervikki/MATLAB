ff65ca0fbf864a933f53519c6f14137bc03551c2
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-09-26 19:23:25 +0530

    A MATLAB program to solve the system of given linear equation using gauss elimination.
    A MATLAB program to solve the system of given linear equation using gauss elimination.
    
    Signed-off-by: Vikash Yadav <91186160+developervikki@users.noreply.github.com>

---
+++ gauss_jordan_method.m
@@ -0,0 +1,23 @@
+function[]=gauss_jordan_method()
+clc;
+clear all;
+n=input('Enter ther no. of variabls');
+A=input('Enter the elements of augmented matrix: ');
+for j=1:1:n
+    for i=1:1:n
+        if(i~=j)
+            t=A(i,j)/A(j,j);
+            for k=1:1:n+1
+                A(i,k)=A(i,k)-t*A(j,k);
+            end
+        end
+    end
+end
+disp('The augmented matrix become:');
+disp(A);
+for i=1:1:n
+    x(i)=A(i,n+1)/A(i,i);
+end
+disp('The soltutions of the given system using gauss elimination method is: ');
+disp(x);
+end
