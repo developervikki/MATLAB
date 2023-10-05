44051ec8b8ed9185de37c3d2d74a9c9be289221c
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-05 20:06:45 +0530

    write a MATLAB program to solve problem using newton forword formula
    write a MATLAB program to solve problem using newton forword formula
    
    Signed-off-by: Vikash Yadav <91186160+developervikki@users.noreply.github.com>

---
+++ newton_forword_formula.m
@@ -0,0 +1,29 @@
+x = [0, 1, 2, 3, 4];
+y = [1, 2, 5, 10, 17];
+
+x_interpolate = 2.5;
+
+n = length(x);
+f = zeros(n, n);
+
+f(:, 1) = y';
+
+for j = 2:n
+    for i = 1:n-j+1
+        f(i, j) = f(i+1, j-1) - f(i, j-1);
+    end
+end
+
+interpolated_value = f(1, 1);
+
+u = (x_interpolate - x(1)) / (x(2) - x(1));
+coefficients = [1];
+for j = 2:n
+    coefficients(j) = coefficients(j-1) * (u - j + 2) / j;
+end
+
+for j = 1:n-1
+    interpolated_value = interpolated_value + coefficients(j) * f(1, j+1);
+end
+
+fprintf('Interpolated value at x = %.2f is %.4f\n', x_interpolate, interpolated_value);
