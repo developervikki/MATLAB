f341961c14566ded9f663ead656d068424d848fc
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-13 21:50:52 +0530

    Write a MATLAB program to solve convolution problem.
    Write a MATLAB program to solve convolution problem.

---
+++ convo.m
@@ -0,0 +1,23 @@
+% Define two input sequences
+x = [1, 2, 3, 4];
+h = [0.5, 0.5];
+
+% Perform convolution
+y = conv(x, h);
+
+% Display the result
+disp('Convolution result:');
+disp(y);
+
+% Plot the input and output sequences
+subplot(3,1,1);
+stem(x, 'r', 'LineWidth', 1.5);
+title('Input Sequence x[n]');
+
+subplot(3,1,2);
+stem(h, 'b', 'LineWidth', 1.5);
+title('Impulse Response h[n]');
+
+subplot(3,1,3);
+stem(y, 'g', 'LineWidth', 1.5);
+title('Convolution Result y[n]');
