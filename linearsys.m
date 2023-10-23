c21323fa4704ed9a477be85b529d7a91e5afe3d8
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-23 19:09:49 +0530

    write a MATLAB program to plot a linear system
    write a MATLAB program to plot a linear system

---
+++ linearsys.m
@@ -0,0 +1,18 @@
+% Define the transfer function
+numerator = 1;
+denominator = [1, 1]; % s + 1
+
+sys = tf(numerator, denominator);
+
+% Generate a time vector for the step response
+t = 0:0.01:10; % Time from 0 to 10 seconds with a 0.01-second step
+
+% Compute the step response of the system
+y = step(sys, t);
+
+% Plot the step response
+plot(t, y);
+xlabel('Time (seconds)');
+ylabel('System Response');
+title('Step Response of a First-Order Linear System');
+grid on;
