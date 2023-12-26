bbebcfe44b69d3e7bd8d3b4ac9c4d07e16c42d5f
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-12-26 15:20:17 +0530

    Update newton_forword_formula.m

--- newton_forword_formula.m
+++ newton_forword_formula.m
@@ -1,3 +1,4 @@
+x=input
 x = [0, 1, 2, 3, 4];
 y = [1, 2, 5, 10, 17];
 
@@ -15,7 +16,7 @@ for j = 2:n
 end
 
 interpolated_value = f(1, 1);
-
+ 
 u = (x_interpolate - x(1)) / (x(2) - x(1));
 coefficients = [1];
 for j = 2:n
