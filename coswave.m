c45c255f33284b2d7ab7052e02124499661ff11b
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-03-31 23:53:47 +0530

    A MATLAB program to generate cos wave..
    A MATLAB program to generate cos wave..

--- coswave.m
+++ coswave.m
@@ -1,18 +1,30 @@
 t=0:0.01:5;
+
 x=cos(2*pi*t);
+
 subplot(3,2,3);
+
 plot(t,x);
+
 title('cos continuous wave');
+
 xlabel('time');
-ylabel('amplitude');
- 
- 
-t=0:0.01:5;
-x=cos(2*pi*t);
-subplot(3,2,4);
-stem(t,x);
-title('cos discrete wave');
-xlabel('time');
+
 ylabel('amplitude');
 
-  
+
+
+t=0:0.01:5;
+
+x=cos(2*pi*t);
+
+subplot(3,2,4);
+
+stem(t,x);
+
+title('cos discrete wave');
+
+xlabel('time');
+
+ylabel('amplitude');
+
