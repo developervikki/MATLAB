81303490dd26e3294802ddb02b987d7c5181a811
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-04-15 23:31:55 +0530

    Update linearsys.m

--- linearsys.m
+++ linearsys.m
@@ -7,7 +7,7 @@ sys = tf(numerator, denominator);
 % Generate a time vector for the step response
 t = 0:0.01:10; % Time from 0 to 10 seconds with a 0.01-second step
 
-% Compute the step response of the system
+% Compute the step response of the system 
 y = step(sys, t);
 
 % Plot the step response
