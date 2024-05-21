d54d5e0d0d8829168ab8c1e50559992ab2c3deed
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-05-21 20:35:41 +0530

    Update Ztrans.m

--- Ztrans.m
+++ Ztrans.m
@@ -4,7 +4,7 @@ x = 0.8.^n; % Define the signal, e.g., x(n) = 0.8^n
  
  
 syms z n;   
-X = ztrans(x, n, z); % Compute the Z-transform of x(n) 
+X = ztrans(x, n, z); % Compute the Z-transform of x(n)  
 
  
 figure; 
